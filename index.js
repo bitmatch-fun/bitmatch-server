f22bea10fa75c173001b79d7a219dbc66925f74a
Sebastian Flores <sebastian@bitmatch.fun>
2025-12-05 06:38:58 -0600

    changes

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ matchMaker.controller.getCorsHeaders = function (req) {
 
 const gameServer = new Server({
   transport: new uWebSocketsTransport({
-    idleTimeout: 900,
+    idleTimeout: 980,
   }),
 });
 
